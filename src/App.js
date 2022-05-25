de3c2fdbf0e35de4291c39fd3ef3749e1aa942fb
sruthybabu <sruthybabu64@gmail.com>
2022-05-25 19:01:52 +0530

    sign in completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Signup from './components/Signup';
+import Signin from './components/Signin';
 
 function App() {
   return (
     <div>
 <Signup/>
+<Signin/>
 
 
     </div>
---
+++ src/components/Signin.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Signin = () => {
+    var [username,setUsername]=useState("")
+    var [password,setPassword]=useState("")
+    const subData=()=>{
+        const data={"userName":username,"password":password}
+        console.log(data)
+    }
+  return (
+    <div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
+                    <div className='row g-3'>
+                        <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
+                           <label for="" className="form-label">Username</label>
+                           <input onChange={(e)=>{setUsername(e.target.value)}} placeholder="Enter your username" type="text" className="form-control"/>
+                        </div>
+                        <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
+                            <label for="" className="form-label">Password</label>
+                            <input onChange={(e)=>{setPassword(e.target.value)}} placeholder="Enter your password" type="password" name="" id="" className="form-control"/>
+                        </div>
+                        <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
+                        <button onClick={subData} className="btn btn-primary">SIGN IN</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Signin
