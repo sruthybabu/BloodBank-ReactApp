2121e256c3225095752a7926b969f5b187997f8f
sruthybabu <sruthybabu64@gmail.com>
2022-05-26 10:27:52 +0530

    complete

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -37,9 +37,9 @@ const Signup = () => {
                     <label for="" className="form-label">Name</label>
                     <input onChange={(e)=>{setName(e.target.value)}} placeholder="Eneter your name" type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Address</label>
-                    <input onChange={(e)=>{setAddress(e.target.value)}} placeholder="Enter your address" type="text" className="form-control"/>
+                    <textarea onChange={(e)=>{setAddress(e.target.value)}} placeholder="Enter your address" name="" id="" cols="30" rows="10" className="form-control"></textarea>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Blood Group</label>
